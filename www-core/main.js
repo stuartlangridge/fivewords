c726fb22689674a1d18e3ca9d6057142f5434e20
Stuart Langridge <sil@kryogenix.org>
2014-07-04 20:29:01 +0100

    More handling of scroll and so on

--- www-core/main.js
+++ www-core/main.js
@@ -1,9 +1,7 @@
 function loadSavedGame(cb) { console.log("in loadSavedGame"); cb(); }
-function loadAnswers(cb) { console.log("in loadAnswers"); cb(); }
-function arrangeScreen(cb) { console.log("in arrangeScreen"); cb(); }
 
 function getInputFocusHandler(inp) {
-    var before = inp.offsetHeight;
+    var before = inp.offsetHeight, root = document.getElementById("root");
     return function(e) {
         if (inp.className == "game") {
             setTimeout(function() {
@@ -14,22 +12,31 @@ function getInputFocusHandler(inp) {
         inp.className = "game";
         setTimeout(function() {
           if (document.body.scrollTop > 0) {
-            inp.style.top = (before - document.body.scrollTop - inp.offsetHeight - 10) + "px";
-            inp.style.bottom = "auto";
+            // Browser does not readjust height of screen when keyboard appears
+            root.style.height = (before - document.body.scrollTop - 10) + "px";
             document.body.scrollTop = 0;
           } else {
+            // Browser readjusts height of screen when keyboard appears
             inp.style.top = inp.offsetTop + "px";
             inp.style.bottom = "auto";
             document.body.scrollTop = 0;
           }
-        }, 0);
+          document.getElementById("coverall").style.display = "none";
+        }, 250);
     };
+}
+
+function chooseLetter(e) {
+    document.querySelector("#clue").innerHTML = e.target.innerHTML;
 }
 
 function attachEventHandlers(cb) {
     var inp = document.getElementById("maininput");
     inp.addEventListener("focus", getInputFocusHandler(inp), false);
+    var myScroll = new IScroll('#letterchooser', { scrollX: true, scrollY: false, mouseWheel: true, tap: true });
+    document.querySelector("#inner").addEventListener("tap", chooseLetter, false);
     document.addEventListener("touchmove", function(e) { e.preventDefault(); }, false);
+    cb();
 }
 
 function inSeriesDo() {
@@ -43,9 +50,7 @@ function inSeriesDo() {
 function startup() {
     inSeriesDo(
         attachEventHandlers,
-        loadSavedGame,
-        loadAnswers,
-        arrangeScreen
+        loadSavedGame
     );
 }
 document.addEventListener("DOMContentLoaded", startup);
